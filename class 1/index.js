cd2c532f6eb152513363dc52df0b80cc30b44d60
Taha Rao <rao917120@gmail.com>
2026-06-11 18:31:34 +0500

    Add initial class 1 files
    Add index.html, index.js, and main.py for a class 1 exercise. index.html provides a basic HTML scaffold that includes index.js. index.js logs name, age, and favorite programming language to the console. main.py prints name, city, and favorite sport. These are simple demo/example files for the assignment.

---
+++ class 1/index.js
@@ -0,0 +1,5 @@
+console.log("My name is Taha Rao")
+
+console.log("I am 15 years old")
+
+console.log("My favorite programming language is JavaScript")
---
+++ class 1/main.py
@@ -0,0 +1,3 @@
+print("My name is Taha Rao")
+print("I am from Karachi")
+print("My favorite sport is football")
